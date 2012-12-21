d5a8deb71d853cbaf059547c01941a6f8cd4f4da
Frank Bültge <frank@bueltge.de>
2012-12-21 12:20:52 +0100

    update wp doc

--- akismet-privacy-policies.php
+++ akismet-privacy-policies.php
@@ -1,32 +1,13 @@
 <?php
 /**
- * @package Akismet Privacy Policies
+ * Plugin Name: Akismet Privacy Policies
+ * Plugin URI:  http://wpde.org/
+ * Description: Ergänzt das Kommentarformular um datenschutzrechtliche Hinweise bei Nutzung des Plugins Akismet.
+ * Version:     1.1.0
+ * Author:      Inpsyde GmbH
+ * Author URI:  http://inpsyde.com/
+ * License:     GPLv3
  */
-/*
-Plugin Name: Akismet Privacy Policies
-Plugin URI: http://wpde.org/
-Description: Ergänzt das Kommentarformular um datenschutzrechtliche Hinweise bei Nutzung des Plugins Akismet.
-Version: 1.1.0
-Author: Inpsyde GmbH
-Author URI: http://inpsyde.com/
-License: GPLv2
-*/
-
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
 
 class Akismet_Privacy_Policies {
 	
@@ -410,4 +391,3 @@ if ( function_exists('add_action') && class_exists('Akismet_Privacy_Policies') )
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
 }
-?>
